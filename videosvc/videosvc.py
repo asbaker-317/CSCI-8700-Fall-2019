f02cd6b433100690d87e429dc1cd42ece1d8b55a
calddub <calobodude@gmail.com>
2019-12-03 06:28:14 -0600

    Thresholding system in place

--- videosvc/videosvc.py
+++ videosvc/videosvc.py
@@ -1,35 +1,46 @@
-#!/usr/bin/python3
 
-
+from imutils.video import VideoStream
 import numpy as np
 import cv2
+import time
 
-cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture(1)
 
-# Define the codec and create VideoWriter object
-#This works on windows - Not on Mac
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-fourcc = cv2.VideoWriter_fourcc(*'X264')
-out = cv2.VideoWriter('output.mov',fourcc, 20.0, (640,480))
+# PiClopse Video Capture object
+class PCVideoCapture:
+	def __init__(self,camnum):
+		self.cam = camnum
+		#self.vc = cv2.VideoCapture(self.cam)
+		self.vs = VideoStream(self.cam).start()
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret==True:
-        #frame = cv2.flip(frame,0)
+class PCVideoRecord:
+	def __init__(self):
+		self.file = None
 
-        # write the flipped frame
-        out.write(frame)
+class PCConfiguration:
+	def __init__(self):
+		self.confmap = None
 
-        #cv2.imshow('frame',frame)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
-    else:
-        break
 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+
+
+#A
+#class Cy
+#class CyVideoCapture( stream ):objectobjectobj
+
+vc = PCVideoCapture( 0 )
+print( "[INFO] VideoCapture initialized. Pausing for cam to stabilize" )
+
+time.sleep(5.0)
+print( "[INFO] Cam stabilized... Begining main processing loop" )
+
+processing = True
+i = 0
+
+while( processing ):
+	print( "[DEBUG] Loop iteration ", i )
+	time.sleep(1.0)
+	i = i + 1
+	if i > 10:
+		processing = False
+
 
